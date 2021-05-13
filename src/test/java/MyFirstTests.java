4bbef4ecfb80b64620a5d03177345c03a46e1780
Roland Valdez <valdez.roland.23@gmail.com>
2021-05-13 16:49:35 -0500

    finished warm up exercise

--- src/test/java/MyFirstTests.java
+++ src/test/java/MyFirstTests.java
@@ -1,4 +1,8 @@
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class MyFirstTests {
@@ -48,4 +52,29 @@ public class MyFirstTests {
 
         assertArrayEquals(expected, actual);
     }
+    @Test
+    public void arrayListNotSame(){
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+
+        assertNotSame(languages, moreLanguages);
+    }
+
+    @Test
+    public void arraysAreEqual(){
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
+        assertArrayEquals(numbers, otherNumbers);
+    }
+    @Test
+    public void trueOrFalse (){
+        String language = "PHP";
+        assertTrue(language.contains("H")); // use assertTrue for this statement
+        assertFalse(language.contains("J")); // use assertFalse for this statement
+
+    }
+
 }
